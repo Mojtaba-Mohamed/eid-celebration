bdfb3bd370341dec4f91b5e3ee7d3c078c1eca46
Mojtaba <mojtaba.mohamed187@gmail.com>
2022-04-24 23:45:56 +0200

    Upload files

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const illustrationContainer = document.getElementById('eid-illustration');
+
+const eidAnimation = bodymovin.loadAnimation({
+    animType: 'svg',
+    wrapper: illustrationContainer, // Required
+    path: 'https://assets1.lottiefiles.com/packages/lf20_ysr762r0.json', // Required
+    loop: true, // Optional
+    autoplay: true // Optional
+});
+
+const fireworkContainers = ['.left-firework', '.right-firework'];
+let container;
+
+function fireworks(){
+    for(let i = 0; i < fireworkContainers.length; i++){
+        container = document.querySelector(fireworkContainers[i]);
+        let animData = {
+            animType: 'svg',
+            wrapper: container,
+            path: 'https://assets8.lottiefiles.com/packages/lf20_obhph3sh.json',
+            loop: false,
+            autoplay: true
+        };
+        
+        let animation = bodymovin.loadAnimation(animData);
+    }
+};
+
+fireworks();
